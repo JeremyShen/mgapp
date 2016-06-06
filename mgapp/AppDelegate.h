758bf30a2b2a8ef2cd12115b4160b51cca357046
makotolusi <lusimingwen@sina.com>
2016-06-06 16:50:39 +0800

    Initial Commit

---
+++ mgapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mgapp
+//
+//  Created by 陆思 on 16/6/6.
+//  Copyright © 2016年 liyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mgapp/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  mgapp
+//
+//  Created by 陆思 on 16/6/6.
+//  Copyright © 2016年 liyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
